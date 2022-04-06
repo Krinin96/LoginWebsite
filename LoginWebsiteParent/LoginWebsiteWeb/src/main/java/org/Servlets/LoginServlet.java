8d40559b33e95bfcbe73db215bb56e64305d10dd
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-06 19:42:52 -0400

    Fixed Servlet class

--- LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/LoginServlet.java
+++ LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/LoginServlet.java
@@ -1,13 +1,18 @@
 package org.Servlets;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import org.LoginWebsiteLogic.User;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
 
-	public void doPost(HttpRequest req, HttpResponse res) {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException {				
+		String username =req.getParameter("username");
+		String password = req.getParameter("password");
+		
 		
 	}
 }
