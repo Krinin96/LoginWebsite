5c47fc7d5d15484d39e2c53050cbe07f7eb741ff
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-06 19:43:48 -0400

    Add AllUsers class
    logic to keep track of all the users

---
+++ LoginWebsiteParent/LoginWebsiteLogic/src/main/java/org/LoginWebsiteLogic/AllUsers.java
@@ -0,0 +1,22 @@
+package org.LoginWebsiteLogic;
+
+
+import java.util.ArrayList;
+
+public class AllUsers {
+
+	private ArrayList<User> users;
+	public AllUsers() {
+		users = new ArrayList<User>();
+	}
+	
+	public void addUser(User user) {
+		users.add(user);
+	}
+	
+	public void removeUser(User user) {
+		for(int i=0; i < users.size(); i++) {
+			if(Users)
+		}
+	}
+}
