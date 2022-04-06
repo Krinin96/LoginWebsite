453fa93b5db52cd956ea04a33e28fd6243b37aa6
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-06 19:03:14 -0400

    Add Login Servlet
    Also added extra Servlet for creating a new user

---
+++ LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/LoginServlet.java
@@ -0,0 +1,13 @@
+package org.Servlets;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import javax.servlet.http.HttpServlet;
+
+public class LoginServlet extends HttpServlet {
+
+	public void doPost(HttpRequest req, HttpResponse res) {
+		
+	}
+}
