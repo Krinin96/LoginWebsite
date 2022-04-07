36dbe3929a91bde2e06c98497b4fe66e69df8f02
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-07 12:52:45 -0400

    Create CreateUserServlet.java

---
+++ LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/CreateUserServlet.java
@@ -0,0 +1,13 @@
+package org.Servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreateUserServlet extends HttpServlet {
+
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+}
