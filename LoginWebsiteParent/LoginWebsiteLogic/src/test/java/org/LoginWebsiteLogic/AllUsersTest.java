2be98a8cefd1f2af8d7695fafda7cfef06d63ef0
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-09 22:06:32 -0400

    Create AllUsersTest.java

---
+++ LoginWebsiteParent/LoginWebsiteLogic/src/test/java/org/LoginWebsiteLogic/AllUsersTest.java
@@ -0,0 +1,12 @@
+package org.LoginWebsiteLogic;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+public class AllUsersTest {
+
+	@Test
+	public void CreatAllUserTest() {
+		AllUsers allUsers = new AllUsers();
+	}
+}
