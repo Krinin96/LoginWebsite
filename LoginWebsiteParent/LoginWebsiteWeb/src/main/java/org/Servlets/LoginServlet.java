a3bbb9fa488126cd2c96b75a77aee8be7fb39dcb
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-12 15:55:23 -0400

    Update LoginServlet.java

--- LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/LoginServlet.java
+++ LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/LoginServlet.java
@@ -4,9 +4,11 @@ import org.LoginWebsiteLogic.User;
 import org.LoginWebsiteLogic.AllUsers;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 public class LoginServlet extends HttpServlet {
 
