7548d39bf1ab43874fff62e6b9fce24a45feffec
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-13 14:58:25 -0400

    Changed to deal with exceptions instead of throwing, added annotations

--- LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/LoginServlet.java
+++ LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/LoginServlet.java
@@ -1,7 +1,12 @@
 package org.Servlets;
 
 import org.LoginWebsiteLogic.User;
+import org.LoginWebsiteDatabaseLogic.UserDBConnection;
 import org.LoginWebsiteLogic.AllUsers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,12 +14,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException {				
+	public void doPost(HttpServletRequest req, HttpServletResponse res)  {				
 		String username =req.getParameter("username");
 		String password = req.getParameter("password");	
+		
+		UserDBConnection dbConnection = new UserDBConnection();
+		
+		
+		PrintWriter out = null;
+		try {
+			out = res.getWriter();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			System.out.print("failed to get writer");
+			e1.printStackTrace();
+		}
+		
+		try {
+			if(dbConnection.checkIfLoginIsCorrect(username, password)) {
+				out.println("Welcome " + username);
+			}else {
+				out.println("User name or password incorrect");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("failed to connect");
+			e.printStackTrace();
+		}
 		
 	}
 }
