d9b48f93056a7436b30ecca2805059af2cd3670c
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-13 14:57:25 -0400

    formatted and annotations

--- LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/CreateUserServlet.java
+++ LoginWebsiteParent/LoginWebsiteWeb/src/main/java/org/Servlets/CreateUserServlet.java
@@ -1,6 +1,5 @@
 package org.Servlets;
 
-
 import org.LoginWebsiteDatabaseLogic.UserDBConnection;
 
 import java.io.IOException;
@@ -13,23 +12,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/CreateNew")
 public class CreateUserServlet extends HttpServlet {
 
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse res)  {
-		String username =req.getParameter("username");
-		String password = req.getParameter("password");	
-		
-		
-	
-		
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+
 		UserDBConnection dbConnection = new UserDBConnection();
-		
+
 		try {
-			if(!dbConnection.checkIfPersonExist(username)) {
+			if (!dbConnection.checkIfPersonExist(username)) {
 				dbConnection.addUser(username, password);
-				
+
 				try {
 					res.sendRedirect("index.html");
 				} catch (IOException e) {
@@ -37,8 +32,8 @@ public class CreateUserServlet extends HttpServlet {
 					System.out.println("cant redirect");
 					e.printStackTrace();
 				}
-			}else {
-				//username taken
+			} else {
+				// username taken
 				PrintWriter out;
 				try {
 					out = res.getWriter();
@@ -48,13 +43,13 @@ public class CreateUserServlet extends HttpServlet {
 					System.out.println("cant print anything");
 					e.printStackTrace();
 				}
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			System.out.println("failed here");
 			e.printStackTrace();
-			
+
 		}
 	}
 }
