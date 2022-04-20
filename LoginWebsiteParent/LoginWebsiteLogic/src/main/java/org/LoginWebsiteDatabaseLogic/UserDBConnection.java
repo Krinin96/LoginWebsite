96d100a52e38c2d6b632ccf57d06d1c22e7f9d28
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-04-20 19:56:36 -0400

    Changed imports and functionality to use hibernate

--- LoginWebsiteParent/LoginWebsiteLogic/src/main/java/org/LoginWebsiteDatabaseLogic/UserDBConnection.java
+++ LoginWebsiteParent/LoginWebsiteLogic/src/main/java/org/LoginWebsiteDatabaseLogic/UserDBConnection.java
@@ -1,121 +1,57 @@
 package org.LoginWebsiteDatabaseLogic;
 
-import org.LoginWebsiteLogic.User;
+import org.LoginWebsiteLogic.*;
 
-import java.sql.Connection;
 
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.SQLException;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 public class UserDBConnection {
 
-	
-	
-	public static void main(String[]args) {
-		
-		User newUser = new User("Steve","Steve123");
-		addUserHibernate(newUser);
-	}
-	
-	
-	private Connection getConnection() {
-
-		try {
-			String url = "jdbc:mysql://localhost:3306/usersdb";
-			String username = "root";
-			String password = "Password";
-
-			Class.forName("com.mysql.cj.jdbc.Driver");
-
-			return DriverManager.getConnection(url, username, password);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("failed to connect to sql");
-			e.printStackTrace();
-
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("class not found");
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public void addUser(String username, String password) throws SQLException {
-
-		Connection connection = getConnection();
-		String sql = "INSERT INTO user (username,password) VALUES (?,?)";
-		PreparedStatement statement = connection.prepareStatement(sql);
-		statement.setString(1, username);
-		statement.setString(2, password);
-		statement.executeUpdate();
-
-		statement.close();
-		connection.close();
-	}
-	
-	public static void addUserHibernate(User user) {
-		
-		
+	public SessionFactory getSessionFactory() {
 		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class);
-		SessionFactory sf = con.buildSessionFactory();
-		
-		Session session = sf.openSession();
+		ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+		SessionFactory sf = con.buildSessionFactory(reg);
+		return sf;
+	}
+
+	public void addUser(User user) {
+
+		Session session = getSessionFactory().openSession();
+
 		Transaction tx = session.beginTransaction();
+
 		session.save(user);
+
 		tx.commit();
 	}
 
-	public boolean checkIfPersonExist(String username) throws SQLException {
-		// gets all from user table
+	public boolean checkIfUserExist(User user) {
+		Session session = getSessionFactory().openSession();
 
-		Connection connection = getConnection();
-		String sql = "SELECT * FROM user";
-		Statement statement = connection.createStatement();
+		if (user.equals((User) session.get(User.class, user.getUserName()))) {
 
-		ResultSet result = statement.executeQuery(sql);
-
-		while (result.next()) {
-
-			if (username.equals(result.getString(2))) {
-
-				connection.close();
-				return true;
-			}
-		}
-
-		connection.close();
-		return false;
-
-	}
-
-	public boolean checkIfLoginIsCorrect(String username, String password) throws SQLException {
-		if (!checkIfPersonExist(username)) {
-			return false;
-		}
-
-		String sql = "SELECT username, password FROM user";
-		Connection connection = getConnection();
-		Statement statement = connection.createStatement();
-		ResultSet result = statement.executeQuery(sql);
-
-		while (result.next()) {
-
-			if (username.equals(result.getString(1)) && password.equals(result.getString(2))) {
-				connection.close();
-				return true;
-
-			}
-
+			return true;
 		}
 
 		return false;
 	}
+
+	public boolean checkIfLoginIsCorrect(User user) {
+		Session session = getSessionFactory().openSession();
+
+		if (user.equals((User) session.get(User.class, user.getUserName()))) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	
 }
